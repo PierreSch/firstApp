f0ee297e4b6b3ae1b45019e045b78ed370c81493
PierreSch <pierreschenberg@yahoo.fr>
2022-05-30 16:48:22 +0200

    -

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class BooksController extends AbstractController
+class BookController extends AbstractController
 {
     /**
      * @Route("/book", name="app_book")
