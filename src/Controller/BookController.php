6b907e1ce718d6b6ce6b1540755a23f9774adb0d
PierreSch <pierreschenberg@yahoo.fr>
2022-05-31 14:39:23 +0200

    -

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -6,15 +6,32 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/book", name="book:")
+ */
 class BookController extends AbstractController
 {
     /**
-     * @Route("/book", name="app_book")
+     * @Route("s", name="index")
      */
     public function index(): Response
     {
         return $this->render('book/index.html.twig', [
             'controller_name' => 'BookController',
         ]);
+    }
+    /**
+     * @Route("", name="create")
+     */
+    public function create(): Response
+    {
+        return $this->render('book/create.html.twig');
+    }
+    /**
+     * @Route("/{id}", name="show")
+     */
+    public function show(): Response
+    {
+
     }
 }
